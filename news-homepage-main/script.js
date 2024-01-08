c233b9bd3470a6001c4f9570abcf260672a88231
HarshBehera <97006041+HarshBehera@users.noreply.github.com>
2024-01-08 18:05:37 +0530

    brightness added

--- news-homepage-main/script.js
+++ news-homepage-main/script.js
@@ -1,14 +1,24 @@
 const navbar = document.querySelector("nav");
+const container = document.querySelector(".container");
+const logo_id = document.querySelector(".logo");
+const attributes = document.querySelector(".attribution");
 const openMenu = document.getElementById("menu-button");
 const closeMenu = document.getElementById("menu-close");
 
 openMenu.addEventListener("click", () => {
     console.log("open working");
+    console.log(logo_id)
   navbar.classList.add("navbar-items");
+  container.style.filter="brightness(50%)";
+  logo_id.style.filter="brightness(50%)";
+  attributes.style.filter="brightness(50%)";
 });
 
 closeMenu.addEventListener("click", () => {
     console.log("close working");
 
   navbar.classList.remove("navbar-items");
+  container.style.filter="brightness(100%)";
+  logo_id.style.filter="brightness(100%)";
+  attributes.style.filter="brightness(100%)";
 });
